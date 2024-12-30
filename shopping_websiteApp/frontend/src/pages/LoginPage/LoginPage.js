b8646db23babe45b5c4eb337d778bce41f7372e8
fouad mahmoud <fouad_31057119@f-eng.tanta.edu.eg>
2024-12-30 14:11:58 +0200

    Update LoginPage.js

--- shopping_websiteApp/frontend/src/pages/LoginPage/LoginPage.js
+++ shopping_websiteApp/frontend/src/pages/LoginPage/LoginPage.js
@@ -1,38 +1,11 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
-import { GoogleLogin } from 'react-google-login';
-import FacebookLogin from 'react-facebook-login';
 import axios from 'axios';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState(''); 
   const [phoneNumber, setPhoneNumber] = useState('');
-
-  const handleGoogleLoginSuccess = async (response) => {
-    try {
-      const res = await axios.post('https://shopping-websiteapp-backend.cloud-stacks.com/api/login/google', {
-        tokenId: response.tokenId,
-      });
-      localStorage.setItem('token', res.data.token);
-      window.location.href = '/dashboard';
-    } catch (error) {
-      console.error('Google login error:', error.response.data.error);
-    }
-  };
-
-  const handleFacebookLoginSuccess = async (response) => {
-    try {
-      const res = await axios.post('https://shopping-websiteapp-backend.cloud-stacks.com/api/login/facebook', {
-        accessToken: response.accessToken,
-        userID: response.userID,
-      });
-      localStorage.setItem('token', res.data.token);
-      window.location.href = '/dashboard';
-    } catch (error) {
-      console.error('Facebook login error:', error.response.data.error);
-    }
-  };
 
   const handleEmailLogin = async (e) => {
     e.preventDefault();
@@ -51,22 +24,6 @@ const LoginPage = () => {
   return (
     <div className="login-page">
       <h1>Login</h1>
-      <div className="social-login">
-        <GoogleLogin
-          clientId="YOUR_GOOGLE_CLIENT_ID"
-          buttonText="Login with Google"
-          onSuccess={handleGoogleLoginSuccess}
-          onFailure={(error) => console.error('Google login error:', error)}
-          cookiePolicy={'single_host_origin'}
-        />
-        <FacebookLogin
-          appId="YOUR_FACEBOOK_APP_ID"
-          autoLoad={false}
-          fields="name,email,picture"
-          callback={handleFacebookLoginSuccess}
-          textButton="Login with Facebook"
-        />
-      </div>
       <form onSubmit={handleEmailLogin} className="email-login">
         <input
           type="email"
